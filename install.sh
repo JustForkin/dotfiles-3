66ab0eba4d67d15249ef39576938517dc0703a2d
ntl <nathanladd+github@gmail.com>
2012-12-24 13:26:18 -0600

    Remove command-t installation from install.sh

--- install.sh
+++ install.sh
@@ -14,7 +14,3 @@ mkdir ~/.vim
 cp -r vim/dot_vim/* ~/.vim
 
 curl -o ~/.git_completion.bash https://raw.github.com/git/git/master/contrib/completion/git-completion.bash
-
-cd ~/.vim/ruby/command-t &&
-  ruby extconf.rb &&
-  make
